15ed59ae739fc3c418106feb72890ea2c627ba3b
hiteshcreator <thehiteshcreator@gmail.com>
2023-06-09 22:22:40 +0530

    Add Fake Data API

--- src/fakeApi/routes.js
+++ src/fakeApi/routes.js
@@ -1,11 +1,24 @@
 const express = require('express')
 
-const getUserData = require('./getjson'); 
+const {getUserData,getPosts} = require('./getjson'); 
 const router  = express.Router()
 
 router.get("/user-data",(req,res,next) => {
 
     const data = getUserData()
+                .then((response)=>{
+                    res.send(response.data)
+                })
+                .catch((error) => {
+                    console.log("error--", error);
+                    res.status(500).send("Internal Server Error");
+                });
+
+})
+
+router.get("/posts",(req,res,next) => {
+
+    const data = getPosts()
                 .then((response)=>{
                     res.send(response.data)
                 })
