4d52a2bb5ef4450bc62eba64e35a1855a3203d66
hiteshcreator <thehiteshcreator@gmail.com>
2023-06-13 22:48:29 +0530

    create a helper file

--- src/twilio/routes.js
+++ src/twilio/routes.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const router  = express.Router()
 const sendTwilioSms = require('./sendSms');
+const {sendResponse} = require('../common/helper');
 
 router.get("/send-sms",(req,res,next) => {
 
     sendTwilioSms((error, response) => {
         if(response){
             console.log(response.body);
-            res.send({
-                message: response.body,
-                status:201
-            })
+            sendResponse(res,response.body);
+            // res.send({
+            //     message: response.body,
+            //     status:201
+            // })
         }
     })
 
